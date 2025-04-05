3b36a33bf85f1a5072a26d4fd6f3e3f21d9c4e4b
Alexandre Trigueros <alexandre.trigueros@changing-chameleon.com>
2025-04-05 16:56:03 +0200

    feat: add tcr on macos

--- install-mac.sh
+++ install-mac.sh
@@ -9,4 +9,7 @@ touch $HOME/.hushlogin
 # Show hidden files in finder
 defaults write com.apple.finder AppleShowAllFiles YES
 
-
+# Install tcr
+sudo mkdir -p /opt/tcr
+curl -s -L https://github.com/murex/TCR/releases/download/v1.4.1/tcr_1.4.1_Darwin_arm64.tar.gz | sudo tar -xvz - -C /opt/tcr
+echo 'PATH=$PATH:/opt/tcr' >>~/.zshr
